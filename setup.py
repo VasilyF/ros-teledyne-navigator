47c3860d99416942947675be07ca20668f273e01
Vasily Fedotov <vasilyfedotov12@gmail.com>
2022-02-03 03:34:18 +0000

    update package to build on Noetic (tested on Ubuntu focal)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Teledyne Navigator DVL ROS package setup."""
 
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
